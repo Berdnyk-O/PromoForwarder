1a72b723e8c4d7ba8b47652c9bf2e628e8d93712
Berdnyk-O <sacha.berdnuk@gmail.com>
2024-02-27 16:18:38 +0200

    add mail and password variables to the configuration

--- PromoForwarder/Program.cs
+++ PromoForwarder/Program.cs
@@ -1,10 +1,21 @@
 ﻿using OpenPop.Mime;
 using PromoForwarder;
+using System.Configuration;
 
-EmailReader reader = new EmailReader();
-EmailForwarder forwarder = new EmailForwarder();
+string? SenderMail = ConfigurationManager.AppSettings["SenderMail"];
+string? SenderMailPass = ConfigurationManager.AppSettings["SenderMailPass"];
+string? RecipientMail = ConfigurationManager.AppSettings["RecipientMail"];
+
+if (string.IsNullOrEmpty(SenderMail) || string.IsNullOrEmpty(SenderMailPass) || string.IsNullOrEmpty(RecipientMail))
+{
+    Console.WriteLine("Error while receiving configuration data");
+    return;
+}
+
+EmailReader reader = new EmailReader(SenderMail, SenderMailPass);
+EmailForwarder forwarder = new EmailForwarder(SenderMail, SenderMailPass);
 
 reader.FindUnreadEmailsMatchingRegex();
 Dictionary<int, Message> messages = reader.Messages;
 
-forwarder.ForwardEmails(messages.Values.ToList());
+forwarder.ForwardEmails(messages.Values.ToList(), RecipientMail);
